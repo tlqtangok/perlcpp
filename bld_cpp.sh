45f44e7a96812b9f4f1c2f0085f63f7c1e60ecd3
tlqtangok <tlqtangok@126.com>
2017-05-14 09:16:59 +0800

    201705140916

--- bld_cpp.sh
+++ bld_cpp.sh
@@ -1,2 +1,4 @@
+export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
+#sudo ln -s libperl.so.5.22 libperl.so
 g++ -o  interp interp.cpp -L`pwd` `perl -MExtUtils::Embed -e ccopts -e ldopts`
 # g++ -std=c++11 -o  interp interp.cpp -L`pwd` `perl -MExtUtils::Embed -e ccopts -e ldopts`
